af44d4bde4bbd4b2e342a8059cbef0969b3bb170
reamfox <sashafarion@gmail.com>
2024-09-25 16:44:28 +0300

    remove unnecessary fields in data objects

--- app/src/main/java/com/weatherapp/freeweather/free/app/mobile/androidapp/weatherandroidonline/data/model/DayData.java
+++ app/src/main/java/com/weatherapp/freeweather/free/app/mobile/androidapp/weatherandroidonline/data/model/DayData.java
@@ -4,41 +4,13 @@ import java.util.List;
 
 public class DayData {
     private String datetime;
-    private long datetimeEpoch;
     private double tempmax;
     private double tempmin;
     private double temp;
-    private double feelslikemax;
-    private double feelslikemin;
-    private double feelslike;
-    private double dew;
     private double humidity;
-    private double precip;
-    private double precipprob;
-    private double precipcover;
-    private List<String> preciptype;
-    private double snow;
-    private double snowdepth;
-    private double windgust;
     private double windspeed;
-    private double winddir;
     private double pressure;
-    private double cloudcover;
-    private double visibility;
-    private double solarradiation;
-    private double solarenergy;
-    private int uvindex;
-    private int severerisk;
-    private String sunrise;
-    private long sunriseEpoch;
-    private String sunset;
-    private long sunsetEpoch;
-    private double moonphase;
-    private String conditions;
-    private String description;
     private String icon;
-    private List<String> stations;
-    private String source;
     private List<HourData> hours;
 
     public String getDatetime() {
@@ -47,14 +19,6 @@ public class DayData {
 
     public void setDatetime(String datetime) {
         this.datetime = datetime;
-    }
-
-    public long getDatetimeEpoch() {
-        return datetimeEpoch;
-    }
-
-    public void setDatetimeEpoch(long datetimeEpoch) {
-        this.datetimeEpoch = datetimeEpoch;
     }
 
     public double getTempmax() {
@@ -81,100 +45,12 @@ public class DayData {
         this.temp = temp;
     }
 
-    public double getFeelslikemax() {
-        return feelslikemax;
-    }
-
-    public void setFeelslikemax(double feelslikemax) {
-        this.feelslikemax = feelslikemax;
-    }
-
-    public double getFeelslikemin() {
-        return feelslikemin;
-    }
-
-    public void setFeelslikemin(double feelslikemin) {
-        this.feelslikemin = feelslikemin;
-    }
-
-    public double getFeelslike() {
-        return feelslike;
-    }
-
-    public void setFeelslike(double feelslike) {
-        this.feelslike = feelslike;
-    }
-
-    public double getDew() {
-        return dew;
-    }
-
-    public void setDew(double dew) {
-        this.dew = dew;
-    }
-
     public double getHumidity() {
         return humidity;
     }
 
     public void setHumidity(double humidity) {
         this.humidity = humidity;
-    }
-
-    public double getPrecip() {
-        return precip;
-    }
-
-    public void setPrecip(double precip) {
-        this.precip = precip;
-    }
-
-    public double getPrecipprob() {
-        return precipprob;
-    }
-
-    public void setPrecipprob(double precipprob) {
-        this.precipprob = precipprob;
-    }
-
-    public double getPrecipcover() {
-        return precipcover;
-    }
-
-    public void setPrecipcover(double precipcover) {
-        this.precipcover = precipcover;
-    }
-
-    public List<String> getPreciptype() {
-        return preciptype;
-    }
-
-    public void setPreciptype(List<String> preciptype) {
-        this.preciptype = preciptype;
-    }
-
-    public double getSnow() {
-        return snow;
-    }
-
-    public void setSnow(double snow) {
-        this.snow = snow;
-    }
-
-    public double getSnowdepth() {
-        return snowdepth;
-    }
-
-    public void setSnowdepth(double snowdepth) {
-        this.snowdepth = snowdepth;
-    }
-
-    public double getWindgust() {
-        return windgust;
-    }
-
-    public void setWindgust(double windgust) {
-        this.windgust = windgust;
     }
 
     public double getWindspeed() {
@@ -185,14 +61,6 @@ public class DayData {
         this.windspeed = windspeed;
     }
 
-    public double getWinddir() {
-        return winddir;
-    }
-
-    public void setWinddir(double winddir) {
-        this.winddir = winddir;
-    }
-
     public double getPressure() {
         return pressure;
     }
@@ -201,132 +69,12 @@ public class DayData {
         this.pressure = pressure;
     }
 
-    public double getCloudcover() {
-        return cloudcover;
-    }
-
-    public void setCloudcover(double cloudcover) {
-        this.cloudcover = cloudcover;
-    }
-
-    public double getVisibility() {
-        return visibility;
-    }
-
-    public void setVisibility(double visibility) {
-        this.visibility = visibility;
-    }
-
-    public double getSolarradiation() {
-        return solarradiation;
-    }
-
-    public void setSolarradiation(double solarradiation) {
-        this.solarradiation = solarradiation;
-    }
-
-    public double getSolarenergy() {
-        return solarenergy;
-    }
-
-    public void setSolarenergy(double solarenergy) {
-        this.solarenergy = solarenergy;
-    }
-
-    public int getUvindex() {
-        return uvindex;
-    }
-
-    public void setUvindex(int uvindex) {
-        this.uvindex = uvindex;
-    }
-
-    public int getSevererisk() {
-        return severerisk;
-    }
-
-    public void setSevererisk(int severerisk) {
-        this.severerisk = severerisk;
-    }
-
-    public String getSunrise() {
-        return sunrise;
-    }
-
-    public void setSunrise(String sunrise) {
-        this.sunrise = sunrise;
-    }
-
-    public long getSunriseEpoch() {
-        return sunriseEpoch;
-    }
-
-    public void setSunriseEpoch(long sunriseEpoch) {
-        this.sunriseEpoch = sunriseEpoch;
-    }
-
-    public String getSunset() {
-        return sunset;
-    }
-
-    public void setSunset(String sunset) {
-        this.sunset = sunset;
-    }
-
-    public long getSunsetEpoch() {
-        return sunsetEpoch;
-    }
-
-    public void setSunsetEpoch(long sunsetEpoch) {
-        this.sunsetEpoch = sunsetEpoch;
-    }
-
-    public double getMoonphase() {
-        return moonphase;
-    }
-
-    public void setMoonphase(double moonphase) {
-        this.moonphase = moonphase;
-    }
-
-    public String getConditions() {
-        return conditions;
-    }
-
-    public void setConditions(String conditions) {
-        this.conditions = conditions;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
     public String getIcon() {
         return icon;
     }
 
     public void setIcon(String icon) {
         this.icon = icon;
-    }
-
-    public List<String> getStations() {
-        return stations;
-    }
-
-    public void setStations(List<String> stations) {
-        this.stations = stations;
-    }
-
-    public String getSource() {
-        return source;
-    }
-
-    public void setSource(String source) {
-        this.source = source;
     }
 
     public List<HourData> getHours() {
--- app/src/main/java/com/weatherapp/freeweather/free/app/mobile/androidapp/weatherandroidonline/data/model/HourData.java
+++ app/src/main/java/com/weatherapp/freeweather/free/app/mobile/androidapp/weatherandroidonline/data/model/HourData.java
@@ -6,28 +6,9 @@ public class HourData {
     private String datetime;
     private long datetimeEpoch;
     private double temp;
-    private double feelslike;
-    private double humidity;
-    private double dew;
-    private double precip;
-    private double precipprob;
-    private double snow;
-    private double snowdepth;
-    private List<String> preciptype;
-    private double windgust;
-    private double windspeed;
-    private double winddir;
-    private double pressure;
-    private double visibility;
-    private double cloudcover;
     private double solarradiation;
-    private double solarenergy;
     private int uvindex;
-    private int severerisk;
-    private String conditions;
     private String icon;
-    private List<String> stations;
-    private String source;
 
     public String getDatetime() {
         return datetime;
@@ -53,132 +34,12 @@ public class HourData {
         this.temp = temp;
     }
 
-    public double getFeelslike() {
-        return feelslike;
-    }
-
-    public void setFeelslike(double feelslike) {
-        this.feelslike = feelslike;
-    }
-
-    public double getHumidity() {
-        return humidity;
-    }
-
-    public void setHumidity(double humidity) {
-        this.humidity = humidity;
-    }
-
-    public double getDew() {
-        return dew;
-    }
-
-    public void setDew(double dew) {
-        this.dew = dew;
-    }
-
-    public double getPrecip() {
-        return precip;
-    }
-
-    public void setPrecip(double precip) {
-        this.precip = precip;
-    }
-
-    public double getPrecipprob() {
-        return precipprob;
-    }
-
-    public void setPrecipprob(double precipprob) {
-        this.precipprob = precipprob;
-    }
-
-    public double getSnow() {
-        return snow;
-    }
-
-    public void setSnow(double snow) {
-        this.snow = snow;
-    }
-
-    public double getSnowdepth() {
-        return snowdepth;
-    }
-
-    public void setSnowdepth(double snowdepth) {
-        this.snowdepth = snowdepth;
-    }
-
-    public List<String> getPreciptype() {
-        return preciptype;
-    }
-
-    public void setPreciptype(List<String> preciptype) {
-        this.preciptype = preciptype;
-    }
-
-    public double getWindgust() {
-        return windgust;
-    }
-
-    public void setWindgust(double windgust) {
-        this.windgust = windgust;
-    }
-
-    public double getWindspeed() {
-        return windspeed;
-    }
-
-    public void setWindspeed(double windspeed) {
-        this.windspeed = windspeed;
-    }
-
-    public double getWinddir() {
-        return winddir;
-    }
-
-    public void setWinddir(double winddir) {
-        this.winddir = winddir;
-    }
-
-    public double getPressure() {
-        return pressure;
-    }
-
-    public void setPressure(double pressure) {
-        this.pressure = pressure;
-    }
-
-    public double getVisibility() {
-        return visibility;
-    }
-
-    public void setVisibility(double visibility) {
-        this.visibility = visibility;
-    }
-
-    public double getCloudcover() {
-        return cloudcover;
-    }
-
-    public void setCloudcover(double cloudcover) {
-        this.cloudcover = cloudcover;
-    }
-
     public double getSolarradiation() {
         return solarradiation;
     }
 
     public void setSolarradiation(double solarradiation) {
         this.solarradiation = solarradiation;
-    }
-
-    public double getSolarenergy() {
-        return solarenergy;
-    }
-
-    public void setSolarenergy(double solarenergy) {
-        this.solarenergy = solarenergy;
     }
 
     public int getUvindex() {
@@ -189,43 +50,11 @@ public class HourData {
         this.uvindex = uvindex;
     }
 
-    public int getSevererisk() {
-        return severerisk;
-    }
-
-    public void setSevererisk(int severerisk) {
-        this.severerisk = severerisk;
-    }
-
-    public String getConditions() {
-        return conditions;
-    }
-
-    public void setConditions(String conditions) {
-        this.conditions = conditions;
-    }
-
     public String getIcon() {
         return icon;
     }
 
     public void setIcon(String icon) {
         this.icon = icon;
-    }
-
-    public List<String> getStations() {
-        return stations;
-    }
-
-    public void setStations(List<String> stations) {
-        this.stations = stations;
-    }
-
-    public String getSource() {
-        return source;
-    }
-
-    public void setSource(String source) {
-        this.source = source;
     }
 }
